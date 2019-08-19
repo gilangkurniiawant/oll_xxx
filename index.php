8b10eb4d183ac4e20391e2965ed59608f9cf10c8
Gilang <gilangkurniawant@gmail.com>
2019-08-19 07:56:35 +0000

    kategori

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
+
 require_once('view/header.php');
 session_start();
-$_SESSION['alert']='';
-$aksi='';
+$_SESSION['alert'] = '';
+$aksi = '';
 
 
-if(@$_GET['action']){
+
+if (@$_GET['action']) {
     $aksi = $_GET['action'];
-    $file ='view/'.$_GET['action'].'.php';
+    $file = 'view/' . $_GET['action'] . '.php';
     require_once('user/action.php');
-    if(file_exists($file)){   
-    require_once($file);
+    if (file_exists($file)) {
+        require_once($file);
     }
-
-} 
+}
 require_once('view/footer.php');
-
-
-
-?>
--- view/dashboard.php
+++ view/dashboard.php
@@ -1,41 +1,45 @@
 <div class="col-md-12">
 
-   <?php
-      function tampil_data($data=array()){
-      ?> 
-   <div class="panel panel-default">
-       
-      <div class="panel-heading">
-         <h3 class="panel-title"><i class="fa fa-history"></i> Daftar Ikan</h3>
-      </div>
-      <div class="panel-body">
-      <a href="index.php?action=add_iklan" class="btn btn-primary">Tambah Iklan</a>
-         <div class="table-responsive">
-            <table class="table table-striped table-bordered table-hover m-0">
-               <thead>
-                  <tr>
-                     <th>ID</th>
-                     <th>Nama</th>
-                     <th>Kategori</th>
-                     <th>Harga</th>
-                     <th>Kondisi</th>
-                     <th>Status</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <?php foreach($data as $d){?>
-                  <tr>
-                     <td><?=$d['id']?></td>
-                     <td><a href="<?=$d['url']?>" target="_blank"><?=$d['title']?></a></td>
-                     <td><?=$d['category_id']?></td>
-                     <td><?=$d['params']['price']['value']?></td>
-                     <td><?=$d['params']['condition']['value']?></td>
-                     <td><?=$d['status']?></td>
-                  </tr>
-                  <?php } }?>
-            </table>
-         </div>
-      </div>
-   </div>
+    <?php
+    function tampil_data($data = array(), $kategori)
+    {
+
+
+        ?>
+    <div class="panel panel-default">
+
+        <div class="panel-heading">
+            <h3 class="panel-title"><i class="fa fa-history"></i> Daftar Ikan</h3>
+        </div>
+        <div class="panel-body">
+            <a href="index.php?action=add_iklan" class="btn btn-primary">Tambah Iklan</a>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover m-0">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Kategori</th>
+                            <th>Harga</th>
+                            <th>Kondisi</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data as $d) { ?>
+                        <tr>
+                            <td><?= $d['id'] ?></td>
+                            <td><a href="<?= $d['url'] ?>" target="_blank"><?= $d['title'] ?></a></td>
+                            <td><?= $kategori[$d['category_id']] ?></td>
+                            <td><?= $d['params']['price']['value'] ?></td>
+                            <td><?= $d['params']['condition']['value'] ?></td>
+                            <td><?= $d['status'] ?></td>
+                        </tr>
+                        <?php }
+                        } ?>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
